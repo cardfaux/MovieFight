6caa41a2f9e204d9d0599f757bd94430f52f82af
James Hagood <cardfaux@tutanota.com>
2020-01-24 19:39:00 -0600

    added api key and that is it

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hi there!');
+const fetchData = async () => {
+	const response = await axios.get('http://www.omdbapi.com/', {
+		params: {
+			apikey: '6bee44fa',
+			i: 'tt0848228'
+		}
+	});
+
+	console.log(response.data);
+};
+fetchData();
